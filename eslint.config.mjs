07d65fa08eea76753ce17b8328f92b2d3ce4301b
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2025-04-26 12:36:53 +0530

    chore: add linting, formatting, and pre-commit hooks
    This commit introduces linting and formatting configurations, including ESLint, Prettier, and Husky pre-commit hooks. It also adds VS Code settings and extensions recommendations to ensure consistent code style and quality across the project.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginJsxA11y from "eslint-plugin-jsx-a11y";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,28 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["**/.next/**", "**/node_modules/**", "**/dist/**", "**/build/**", "**/out/**"],
+  },
+  { plugins: { "jsx-a11y": eslintPluginJsxA11y } },
+  ...compat.config({
+    extends: ["plugin:jsx-a11y/strict", "next", "prettier"],
+    rules: {
+      semi: ["error"],
+      "no-console": ["warn"],
+      "no-unused-vars": [
+        "warn",
+        {
+          args: "none", // don't warn about unused function args
+          varsIgnorePattern: "^_", // ignore vars starting with _
+          argsIgnorePattern: "^_", // ignore args starting with _
+        },
+      ],
+      "prefer-const": ["error"],
+      "jsx-quotes": ["error", "prefer-double"],
+      quotes: ["error", "double"],
+    },
+  }),
 ];
 
 export default eslintConfig;
